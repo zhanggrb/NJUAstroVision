83ea68a1c3c4c99f4016811880bd387326ca7ace
binbinzhang <francowiderman@gmail.com>
2024-10-14 20:37:33 +0800

    Initial Commit

---
+++ NJUAstroVision/NJUAstroVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NJUAstroVisionApp.swift
+//  NJUAstroVision
+//
+//  Created by bz on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NJUAstroVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NJUAstroVisionTests/NJUAstroVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  NJUAstroVisionTests.swift
+//  NJUAstroVisionTests
+//
+//  Created by bz on 10/14/24.
+//
+
+import Testing
+@testable import NJUAstroVision
+
+struct NJUAstroVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
